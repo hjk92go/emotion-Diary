9ea8f3ab252917384b1fe4f60db83920a8c29194
hjk92go <113073075+hjk92go@users.noreply.github.com>
2023-12-15 13:08:54 +0900

    231215 page구현-일기 상세(/diary)

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -6,69 +6,12 @@ import MyHeader from ".//MyHeader";
 import EmotionItem from "./EmotionItem";
 import { DiaryDispatchContext } from "../App";
 
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
+
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "완전 좋음",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "좋음",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁨",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "끔직함",
-  },
-];
 
-const getStringDate = (date) => {
-  //객체의 toISOString은 ISO 형식의 문자를 반환하는 메서드
-  return date.toISOString().slice(0, 10);
-
-  /**
- * 
- *수업중에 안되면 수정해볼것
- *    let year = date.getFullYear();
-
-  let month = date.getMonth() + 1;
-
-  let day = date.getDate();
-
-  if (month < 10) {
-
-    month = `0${month}`;
-
-  }
-
-  if (day < 10) {
-
-    day = `0${day}`;
-
-  }
-
-  return `${year}-${month}-${day}`;
-
-};
- * 
- * 
- * 
- */
-};
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
   const [content, setContent] = useState("");
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,19 +1,68 @@
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
+import { emotionList } from "../util/emotion";
+import { getStringDate } from "../util/date";
 
 const Diary = () => {
-  //const id는 useParams라는 React Router dom 이 제공하는 함수를 사용할 것
-  //useParams를 이용을 하면 이렇게 전달받아서 들어오게 되는 path variable들을 모아서 객체로 갖다 주게 되는데
-  //우리는 이 variable을 id라고 부르기로 App.js에서 선언함
-  //앞에서 선언한 path variable을 다이어리 컴포넌트에서 id로 꺼내와야한다.
-
   const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
 
-  return (
-    <div>
-      <h2>Diary</h2>
-      <p>상세 페이지</p>
-    </div>
-  );
+  //id와 일치 하는 상세 페이지에서 보여줘야하는 데이터를 찾아서 일기가 존재 할때 state에 할당
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id));
+
+      if (targetDiary) {
+        //일기가 존재할 때
+        setData(targetDiary);
+      } else {
+        //일기 존재X
+        alert("없는 일기 입니다.");
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  //not data
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다.......</div>;
+  } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))}의 기록`}
+          leftChild={<MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />}
+          rightChild={<MyButton text={"수정하기"} onclick={() => navigate(`/edit/${data.id}`)} />}
+        />
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div className={["diary_img_wrapper", `diary_img_wrapper_${data.emotion}`].join(" ")}>
+              <img src={curEmotionData.emotion_img} />
+              <div className="emotion_descript">{curEmotionData.emotion_descript}</div>
+            </div>
+          </section>
+          <section>
+            <h4>오늘의 일기</h4>
+            <div className="diary_content_wrapper">
+              <p>{data.content}</p>
+            </div>
+          </section>
+        </article>
+      </div>
+    );
+  }
 };
 
 export default Diary;
